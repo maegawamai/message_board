de033cd60045e35d5d63e4d34af62630dac01657
mai.maegawa <mai.maru0214@icloud.com>
2022-02-21 20:56:46 +0900

    DestroyServlet.javaを作成

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -25,7 +25,6 @@ public class UpdateServlet extends HttpServlet {
      */
     public UpdateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -36,33 +35,30 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            //editServletのセッションスコープからメッセージのIDを取得して
-            //該当のIDのメッセージ1件のみをデーターベースから取得
-            //データはobject型なのでIntegerへキャストする
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id)")));
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
-            //フォームの内容を各フィールドに上書き
-            String title =request.getParameter("title");
+            // フォームの内容を各フィールドに上書き
+            String title = request.getParameter("title");
             m.setTitle(title);
 
             String content = request.getParameter("content");
             m.setContent(content);
 
-            Timestamp correntTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(correntTime);
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
-            //データベースを更新
+            // データベースを更新
             em.getTransaction().begin();
             em.getTransaction().commit();
             em.close();
 
-            //セッションスコープ上の不要になったデータを削除
+            // セッションスコープ上の不要になったデータを削除
             request.getSession().removeAttribute("message_id");
 
-            //indexページへリダイレクト
-            response.sendRedirect(request.getContextPath()+"/index");;
-
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 }
